6e15c9859bf69e994728d640edf0033495c8b020
nbnmhkjh <rsjcian.hendley@hotmail.com>
2024-05-07 18:26:26 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-function minPathSum(grid) {
-  const rows = grid.length;
-  const cols = grid[0].length;
-  for (let i = 1; i < rows; i++) {
-    grid[i][0] += grid[i - 1][0];
-  }
-  for (let j = 1; j < cols; j++) {
-    grid[0][j] += grid[0][j - 1];
-  }
-  for (let i = 1; i < rows; i++) {
-    for (let j = 1; j < cols; j++) {
-      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
-    }
-  }
-  return grid[rows - 1][cols - 1];
-}
+const factorial = (num) => {
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
+};
