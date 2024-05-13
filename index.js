84c0ec1283bd2c00a83a79ca7fe8b8ef86d5e11d
nbnmhkjh <rsjcian.hendley@hotmail.com>
2024-05-13 12:00:06 +0700

    commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-function isSubsequence(s, t) {
-  let i = 0;
-  let j = 0;
-  while (i < s.length && j < t.length) {
-    if (s[i] === t[j]) i++;
-    j++;
+function permute(nums) {
+  const result = [];
+  backtrack([]);
+  return result;
+  function backtrack(current) {
+    if (current.length === nums.length) {
+      result.push([...current]);
+      return;
+    }
+    for (const num of nums) {
+      if (current.includes(num)) continue;
+      current.push(num);
+      backtrack(current);
+      current.pop();
+    }
   }
-  return i === s.length;
 }
